239fe20550802700d5b8a544cd7f3318611eeeec
Eduard.Boriskin <Boriskin.ee@yandex.ru>
2022-08-29 23:34:33 +0300

    dz

--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -1,35 +1,29 @@
 package course.concurrency.m3_shared.immutable;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Map;
 
 public class OrderService {
 
-    private final ConcurrentHashMap<Long, AtomicReference<Order>> currentOrders = new ConcurrentHashMap<>();
+    private final Map<Long, Order> currentOrders = new HashMap<>();
 
     public long createOrder(List<Item> items) {
         Order order = new Order(items);
-        currentOrders.put(order.getId(), new AtomicReference<>(order));
+        currentOrders.put(order.getId(), order);
         return order.getId();
     }
 
     public void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        Order current, recent;
-        do {
-            current = currentOrders.get(orderId).get();
-            recent = current.withPaymentInfo(paymentInfo);
-        } while (!currentOrders.get(orderId).compareAndSet(current, recent));
-        deliver(recent);
+        Order current = currentOrders.get(orderId).withPaymentInfo(paymentInfo);
+        currentOrders.put(orderId, current);
+        deliver(current);
     }
 
     public void setPacked(long orderId) {
-        Order current, recent;
-        do {
-            current = currentOrders.get(orderId).get();
-            recent = current.doPack();
-        } while (!currentOrders.get(orderId).compareAndSet(current, recent));
-        deliver(recent);
+        Order current = currentOrders.get(orderId).doPack();
+        currentOrders.put(orderId, current);
+        deliver(current);
     }
 
     private void deliver(Order order) {
@@ -37,14 +31,11 @@ public class OrderService {
            return;
         }
 
-        Order current, recent;
-        do {
-            current = currentOrders.get(order.getId()).get();
-            recent = current.withStatus(Order.Status.DELIVERED);
-        } while (!currentOrders.get(order.getId()).compareAndSet(current, recent));
+        Order  current = currentOrders.get(order.getId()).withStatus(Order.Status.DELIVERED);
+        currentOrders.put(order.getId(), current);
     }
 
     public boolean isDelivered ( long orderId){
-        return currentOrders.get(orderId).get().getStatus().equals(Order.Status.DELIVERED);
+        return currentOrders.get(orderId).getStatus().equals(Order.Status.DELIVERED);
     }
 }
